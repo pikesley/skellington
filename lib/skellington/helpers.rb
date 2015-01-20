af1247bd14d0fff79cbf0d1b5434d9db6da811ad
pikesley <github@orgraphone.org>
2015-01-20 18:30:40 +0000

    Simpler

--- lib/skellington/helpers.rb
+++ lib/skellington/helpers.rb
@@ -1,7 +1,5 @@
 module Skellington
   def self.camelise worm_case
-    parts = worm_case.split '_'
-    parts.map! { |word| "#{word[0].upcase}#{word[1..-1]}" }
-    parts.join ''
+    worm_case.split('_').map! { |word| "#{word[0].upcase}#{word[1..-1]}" }.join ''
   end
 end
