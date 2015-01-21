51f45a40f165216942872117876506aa1a3b65db
pikesley <github@orgraphone.org>
2015-01-21 18:43:12 +0000

    Remove leading ./

--- lib/skellington/helpers.rb
+++ lib/skellington/helpers.rb
@@ -2,4 +2,9 @@ module Skellington
   def self.camelise worm_case
     worm_case.split('_').map! { |word| "#{word[0].upcase}#{word[1..-1]}" }.join ''
   end
+
+  def self.unslash word
+    return word[2..-1] if word[0..1] == './'
+    word
+  end
 end
--- spec/skellington_spec.rb
+++ spec/skellington_spec.rb
@@ -5,5 +5,10 @@ module Skellington
     it 'camelises a worm_case string' do
       expect(Skellington.camelise 'worm_case').to eq 'WormCase'
     end
+
+    it 'removes a leading ./' do
+      expect(Skellington.unslash './word').to eq 'word'
+      expect(Skellington.unslash 'another_word').to eq 'another_word'
+    end
   end
 end
