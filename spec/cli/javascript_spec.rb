1fc56d548446822d3a7755342254e43b6b486b59
pikesley <github@orgraphone.org>
2016-01-11 22:44:56 +0000

    Almost there

---
+++ spec/cli/javascript_spec.rb
@@ -0,0 +1,42 @@
+module Skellington
+  describe CLI do
+    let :subject do
+      described_class.new
+    end
+
+    it 'generates javascript files' do
+      subject.generate 'dummy_app'
+      expect('dummy_app/spec/javascripts/support/jasmine.yml').to contain (
+      """
+      src_files:
+        - public/js/**/*.js
+      stylesheets:
+        - stylesheets/**/*.css
+      helpers:
+        - helpers/**/*.js
+      spec_files:
+        - '**/*[sS]pec.js'
+      src_dir:
+      spec_dir:
+      spec_helper: spec/javascripts/support/jasmine_helper.rb
+      boot_dir:
+      boot_files:
+      server: 'puma'
+      random: true
+      """
+      )
+
+      expect(File).to exist 'dummy_app/spec/javascripts/support/jasmine_helper.rb'
+
+      expect('dummy_app/spec/javascripts/dummy_appSpec.js').to contain (
+      """
+      describe('DummyApp', function() {
+        it('knows the truth', function() {
+          expect(true).toEqual(true)
+        })
+      })
+      """
+      )
+    end
+  end
+end
