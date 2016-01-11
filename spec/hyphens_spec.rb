7f4e4a23af20a799162d17cb5e1a4423bf052891
pikesley <github@orgraphone.org>
2016-01-11 20:49:33 +0000

    More stuff

---
+++ spec/hyphens_spec.rb
@@ -0,0 +1,33 @@
+module Skellington
+  describe CLI do
+    let :subject do
+      described_class.new
+    end
+
+    it 'has a version' do
+      expect { subject.version }.to output(/^skellington version #{VERSION}$/).to_stdout
+    end
+
+    it 'generates correct output' do
+      expect { subject.generate 'hyphenated-name' }.to output(/
+Your new Sinatra app HyphenatedName has been created
+
+\(Note that 'hyphenated-name' has been changed to 'hyphenated_name' because Ruby finds '-'s troubling\)
+
+Now do
+
+    cd hyphenated_name
+    bundle
+    bundle exec rake
+
+And presuming that passes OK
+
+    git add .
+    git commit -m 'First commit'
+
+You can run the app with
+
+    bundle exec rackup/).to_stdout
+    end
+  end
+end
