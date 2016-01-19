896c94b03856ea81de69497af90457ff7dd2f205
pikesley <github@orgraphone.org>
2016-01-19 20:58:09 +0000

    badger

--- lib/skellington/cli.rb
+++ lib/skellington/cli.rb
@@ -18,22 +18,30 @@ module Skellington
 
     desc 'postinstall', 'Show some helpful hints'
     def postinstall
-      s = 'Some other things you might find useful (sweeten to taste):'
-      s << "\n\n"
-      s << '* Set up continuous integration in Travis:'
-      s << "\n\n"
-      s << '    travis init'
-      s << "\n\n"
-      s << '* Configure a Heroku app:'
-      s << "\n\n"
-      s << "    heroku apps:create YOUR-APP-NAME-HERE --region=eu"
-      s << "\n\n"
-      s << '* Get Travis to continuously deploy to Heroku:'
-      s << "\n\n"
-      s << '    travis setup heroku'
-      s << "\n\n"
-      s << "(This all depends on you having Travis and Heroku installed and configured)"
+      s = <<HEREDOC
+Some other things you might find useful (sweeten to taste):
 
+* Set up continuous integration in Travis:
+
+    gem install travis
+    travis init
+    git add .travis.yml
+
+* Configure a Heroku app:
+
+    gem install heroku
+    heroku apps:create YOUR-APP-NAME-HERE --region=eu
+
+* Get Travis to continuously deploy to Heroku:
+
+    travis setup heroku
+
+* Add some badges to your README
+
+    gem install badgerbadgerbadger
+    badger > README.md
+    git add README.md
+HEREDOC
       puts s
     end
   end
