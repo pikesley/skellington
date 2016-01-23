f028b52bd1ce17f018266248fe39f80e2f4d029c
pikesley <github@orgraphone.org>
2016-01-23 15:53:45 +0000

    Much tidier

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -43,33 +43,9 @@ For post-install hints, try
     end
 
     it 'gives helpful post-generate advice' do
-      expect { subject.postinstall }.to output(/Some other things you might find useful \(sweeten to taste\):
+      expect { subject.postinstall }.to output(/For some other things you might find useful, see this gist:
 
-\* Install jQuery and jasmine-jQuery \(yes, this is an ugly hack\)
-
-    curl https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1\.11\.1\/jquery.min.js -o spec\/javascripts\/helpers\/jquery.min.js
-    curl https:\/\/raw.github.com\/velesin\/jasmine-jquery\/master\/lib\/jasmine-jquery.js -o spec\/javascripts\/helpers\/load-after-jquery-jasmine-jquery.js
-
-\* Set up continuous integration in Travis:
-
-    gem install travis
-    travis init
-    git add .travis.yml
-
-\* Configure a Heroku app:
-
-    gem install heroku
-    heroku apps:create YOUR-APP-NAME-HERE --region=eu
-
-\* Get Travis to continuously deploy to Heroku:
-
-    travis setup heroku
-
-\* Add some badges to your README
-
-    gem install badgerbadgerbadger
-    badger > README.md
-    git add README.md
+    https:\/\/gist.github.com\/pikesley\/1789cab7b10f0d8765d4
 /).to_stdout
     end
   end
