19d18ec207965cb0bd929e11f69ecae010464e3c
Sam Pikesley <sam.pikesley@gmail.com>
2018-02-07 22:14:29 +0000

    Helpers for JS libs

--- lib/skellington/helpers.rb
+++ lib/skellington/helpers.rb
@@ -7,4 +7,21 @@ module Skellington
     return word[2..-1] if word[0..1] == './'
     word
   end
+
+  def self.js_for_sinatra data
+    s = ['  %s: {' % data['name']]
+    s.push "    url:  '%s'," % (data['url'].sub 'VERSION', data['version'])
+    s.push "    hash: '%s'" % data['hash']
+    s.push '  }'
+
+    s.join "\n"
+  end
+
+  def self.js_for_jekyll data
+    s = ['  - name: %s' % data['name']]
+    s.push "    url:  %s" % (data['url'].sub 'VERSION', data['version'])
+    s.push "    hash: %s" % data['hash'] if data['hash']
+
+    s.join "\n"
+  end
 end
--- spec/skellington_spec.rb
+++ spec/skellington_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 module Skellington
   describe Skellington do
     it 'camelises a worm_case string' do
@@ -9,6 +7,37 @@ module Skellington
     it 'removes a leading ./' do
       expect(Skellington.unslash './word').to eq 'word'
       expect(Skellington.unslash 'another_word').to eq 'another_word'
+    end
+
+    it 'generates a JS lib import line for Sinatra' do
+      data = YAML.load("""
+- name:    bootstrap
+  url:     //maxcdn.bootstrapcdn.com/bootstrap/VERSION/js/bootstrap.min.js
+  version: 4.0.0
+  hash:    sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl
+      """)[0]
+
+      expect(Skellington.js_for_sinatra data).to eq (
+        """  bootstrap: {
+    url:  '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+    hash: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl'
+  }"""
+      )
+    end
+
+    it 'generates a JS lib import line for Jekyll' do
+      data = YAML.load("""
+- name:    bootstrap
+  url:     //maxcdn.bootstrapcdn.com/bootstrap/VERSION/js/bootstrap.min.js
+  version: 4.0.0
+  hash:    sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl
+      """)[0]
+
+      expect(Skellington.js_for_jekyll data).to eq (
+        """  - name: bootstrap
+    url:  //maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
+    hash: sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"""
+      )
     end
   end
 end
